f9c2a2dd04aea311d211dbe8fc6e9cc16ea81c9a
erikcs <erik.sverdrup@gmail.com>
2018-12-29 22:56:52 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import pysecedgar
 setup(name='pysecedgar',
       version='0.1',
       description='pysecedgar',
-      url='https://github.com/nuffe/pysecedgar',
-      author='nuffe',
+      url='https://github.com/erikcs/pysecedgar',
+      author='erikcs',
       license='MIT',
       py_modules=['pysecedgar'])
